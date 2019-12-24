d6b2c96acf3951a909d40c6bb0c158317ae7ddfc
Mohit N Rajput <41369056+mohit-n-rajput@users.noreply.github.com>
2019-12-24 21:12:24 +0530

    Add files via upload

---
+++ python/functions/example.py
@@ -0,0 +1,12 @@
+"""
+This is example module
+"""
+
+
+def add(a, b):
+    """
+
+    This function gives a sum of two numbers. 
+
+    """
+    return a+b
---
+++ python/functions/python_debug.py
@@ -0,0 +1,14 @@
+
+import pdb
+
+# interactive debugging
+
+
+def seq(n):
+    for i in range(n):
+        pdb.set_trace()
+        print(i)
+    return
+
+
+seq(5)
